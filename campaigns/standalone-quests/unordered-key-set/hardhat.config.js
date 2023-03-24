d6ecee0a96278f1f884133ef140276956725ee65
Chase Brown <Chasebrown7@icloud.com>
2023-03-23 19:00:03 -0700

    Download quest unordered-key-set

---
+++ campaigns/standalone-quests/unordered-key-set/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.16",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+  },
+};
