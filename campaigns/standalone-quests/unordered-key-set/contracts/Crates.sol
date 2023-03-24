5f071671632ed2c9b1eacddb2f9358067498d1e5
Chase Brown <Chasebrown7@icloud.com>
2023-03-23 19:33:59 -0700

    #unordered-key-pair

--- campaigns/standalone-quests/unordered-key-set/contracts/Crates.sol
+++ campaigns/standalone-quests/unordered-key-set/contracts/Crates.sol
@@ -4,5 +4,53 @@ pragma solidity ^0.8.16;
 import './interfaces/ICrates.sol';
 
 contract Crates is ICrates {
-    // CODE HERE
+
+    struct Crate {
+        uint index;
+        uint size;
+        uint strength;
+    }
+
+    mapping(uint => Crate) private crates;
+    uint[] private crateIds;
+
+    /// @notice Inserts a crate into the contract. Fails if id belongs to an existing crate.
+    function insertCrate(uint id, uint size, uint strength) external {
+        require(!_isCrate(id));
+
+        crates[id].index = crateIds.length;
+        crates[id].size = size;
+        crates[id].strength = strength;
+        crateIds.push(id);
+    }
+
+    /// @notice Retrieves a crate based on id. Fails if id does not belong to an existing crate.
+    function getCrate(uint id) external view returns (uint size, uint strength) {
+        require(_isCrate(id));
+
+        size = crates[id].size;
+        strength = crates[id].strength;
+    }
+
+    /// @notice Retrieve the IDs of all existing crates.
+    function getCrateIds() external view returns (uint[] memory) {
+        return crateIds;
+    }
+
+    /// @notice Delete a crate by id. Fails if id doesn't belong to an existing crate.
+    function deleteCrate(uint id) external {
+        require(_isCrate(id));
+
+        uint where = crates[id].index;
+        uint length = crateIds.length;
+
+        crates[id].index = where;
+        crateIds[where] = crateIds[length - 1];
+        crateIds.pop();
+    }
+
+    function _isCrate(uint id) internal view returns (bool) {
+        if (crateIds.length == 0) return false;
+        return crateIds[crates[id].index] == id;
+    }
 }
