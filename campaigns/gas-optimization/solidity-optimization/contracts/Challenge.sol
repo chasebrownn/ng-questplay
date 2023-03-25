a30f115cd1011fa5d2dc172115251b1cbee80dac
Chase Brown <Chasebrown7@icloud.com>
2023-03-25 16:20:53 -0700

    #gas-optimization

--- campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
+++ campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.16;
 
 contract Challenge {
 
-    uint256 SKIP;
+    uint256 immutable SKIP;
 
     constructor(uint256 skip) {
         SKIP = skip;
@@ -14,12 +14,26 @@ contract Challenge {
      * @param array The array to sum.
      * @return sum The sum of all the elements of the array excluding SKIP.
      */
-    function sumAllExceptSkip(
-        uint256[] calldata array
-    ) public view returns (uint256 sum) {
+    function sumAllExceptSkip(uint256[] calldata array) public view returns (uint256 sum) {
+        uint256 len = array.length;
+        uint256 skipper = SKIP;
+        uint256 sumThis;
 
-        // IMPLEMENT HERE
+        for (uint256 i; i < len;) {
+
+            sumThis = array[i];
+
+            if (sumThis != skipper) {
+                sum += sumThis;
+            }
+            
+            unchecked {
+                i = i + 1;
+            }
+        }
         
     }
+
+    // 4553 -> 2550
 
 }
