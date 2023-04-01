d3ba28a8e5a5155ecc122ae5675fe34ff59ad194
Chase Brown <Chasebrown7@icloud.com>
2023-03-31 21:59:18 -0700

    Committing the uncommitted

--- campaigns/proxy-contracts/basic-proxies/contracts/UpgradeableMechSuit.sol
+++ campaigns/proxy-contracts/basic-proxies/contracts/UpgradeableMechSuit.sol
@@ -17,25 +17,6 @@ contract UpgradeableMechSuit {
         implementation = _implementation;
     }
 
-    fallback() external {
-        assembly {
-            let ptr := mload(0x40)
-
-            // (1) copy incoming calldata into memory
-            calldatacopy(ptr, 0, calldatasize())
-
-            // (2) forward call to implementation contract
-            let result := delegatecall(gas(), _impl, ptr, calldatasize(), 0, 0)
-            let size := returndatasize()
-
-            // (3) retrieve return data
-            returndatacopy(ptr, 0, size)
-
-            // (4) forward return data back to caller
-            switch result
-            case 0 { revert(ptr, size) }  // Revert if call failed
-            default { return(ptr, size) } // Return if call succeeded
-        }
-    }
+    fallback() external {}
 
 }
