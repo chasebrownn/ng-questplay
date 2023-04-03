8dd3dd5fc0f6361262eb1d7d7d25c40dd16ec622
Chase Brown <Chasebrown7@icloud.com>
2023-04-03 15:50:30 -0700

    #chainlink-vrf

--- campaigns/randomness/chainlink-vrf/contracts_/WizardTower.sol
+++ campaigns/randomness/chainlink-vrf/contracts_/WizardTower.sol
@@ -10,14 +10,16 @@ contract WizardTower is VRFConsumerBaseV2 {
     
     uint32 public floorsClimbed;
 
-    constructor() VRFConsumerBaseV2(COORDINATOR) { }
+    constructor() VRFConsumerBaseV2(COORDINATOR) {
+        floorsClimbed = 8;
+    }
 
     function climb(
-        bytes32 step1,
-        uint64 step2,
-        uint16 step3,
-        uint32 step4,
-        uint32 step5
+        bytes32 step1, // 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15  <- key hash
+        uint64 step2,  // 11108  <- subscription ID
+        uint16 step3,  // 20     <- num blocks to confirm
+        uint32 step4,  // 50000  <- gas limt
+        uint32 step5   // 8      <- num of words requested
     ) external {
         VRFCoordinatorV2Interface(COORDINATOR).requestRandomWords(
             step1,
@@ -36,3 +38,5 @@ contract WizardTower is VRFConsumerBaseV2 {
     }
 
 }
+
+// Deployed to: 0xa57464ad90d32f6aF52e44C62D7f586Aecb17833
