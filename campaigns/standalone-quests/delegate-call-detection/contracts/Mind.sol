4edf288859abda2882aba5b9e58572318645a73b
Chase Brown <Chasebrown7@icloud.com>
2023-04-03 14:27:34 -0700

    #delegate-call-detection

--- campaigns/standalone-quests/delegate-call-detection/contracts/Mind.sol
+++ campaigns/standalone-quests/delegate-call-detection/contracts/Mind.sol
@@ -2,12 +2,20 @@
 pragma solidity ^0.8.16;
 
 contract Mind {
+
+    address immutable thisContract;
+
     /// @notice This could be useful...
-    constructor() {}
+    constructor() {
+        thisContract = address(this);
+    }
 
     /// @notice Checks if the current call is a delegate call.
     /// @return isDelegate true if this is a delegate call, false otherwise.
     function isDelegateCall() public view returns (bool isDelegate) {
-        
+        address(this) == thisContract ? 
+            isDelegate = false : // if match, direct call
+            isDelegate = true ;  // if no match, delegate call
+
     }
 }
