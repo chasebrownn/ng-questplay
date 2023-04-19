57f92582e47702fdcfe18ee795f7cf0d50631c9b
Chase Brown <Chasebrown7@icloud.com>
2023-04-18 20:16:31 -0700

    Changed to IF

--- campaigns/learning-assembly/yul-basics/contracts/If.sol
+++ campaigns/learning-assembly/yul-basics/contracts/If.sol
@@ -8,17 +8,16 @@ contract If {
     /// @param _minutes the number of minutes to convert to hours.
     /// @return _hours the number of hours represented by _minutes.
     function minutesToHours(int256 _minutes) public pure returns (uint256 _hours) {
-        // revert if _minutes is < 0
-        // revert if not divisible by 60
         assembly {
             let success := false
 
-            if or(gt(_minutes, 60), eq(_minutes, 60)) {
-                if eq(mod(60, _minutes), 0) {
-                    _hours := div(_minutes, 60)
-                    success := true
-                }
+            if or(slt(_minutes, 0), sgt(mod(60, _minutes), 0)) {
+                revert(0,0)
             }
+
+            _hours := div(_minutes, 60)
+            success := true
+            
 
             if eq(success, false) {
                 revert(0,0)
